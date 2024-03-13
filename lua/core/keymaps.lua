e87c6400810f1cf2bd69b255eca55593b7981d3c
Axel Snaer <hab123321123@gmail.com>
2024-03-13 10:30:34 +0000

    Add quit all shortcut

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,9 +1,14 @@
 
+-- Save and quit all windows
+vim.keymap.set('n', '<leader>q', ":wqa<cr>")
+
+-- Window navigation
 vim.keymap.set('n', '<C-h>', "<cmd>wincmd h<cr>")
 vim.keymap.set('n', '<C-j>', "<cmd>wincmd j<cr>")
 vim.keymap.set('n', '<C-k>', "<cmd>wincmd k<cr>")
 vim.keymap.set('n', '<C-l>', "<cmd>wincmd l<cr>")
 
+-- Window resizing
 vim.keymap.set("n", "<C-Up>", "<cmd>resize -2<cr>")
 vim.keymap.set("n", "<C-Down>", "<cmd>resize +2<cr>")
 vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>")
